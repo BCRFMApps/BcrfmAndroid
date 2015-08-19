e2f49b2ad75de70f0f3aa1f3ea843b039a498e64
Patrick Melia <patrickmelia82@gmail.com>
2015-08-19 23:43:49 +0100

    Facebook Native App
    Trying to get facebook navtie app to open....only if its installed.

--- BCRFM/app/src/main/java/com/patrickmelia/bcrfm/Fragments/FbookFragment.java
+++ BCRFM/app/src/main/java/com/patrickmelia/bcrfm/Fragments/FbookFragment.java
@@ -1,6 +1,9 @@
 package com.patrickmelia.bcrfm.Fragments;
 
 import android.app.Fragment;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -34,6 +37,18 @@ public class FbookFragment extends Fragment {
                 return false;
             }
         });
+
+
         return view;
     }
+
+    /*public static Intent getOpenFacebookIntent(Context context) {
+
+        try {
+            context.getPackageManager().getPackageInfo("com.facebook.katana", 0);
+            return new Intent(Intent.ACTION_VIEW, Uri.parse("fb://page/181482268700979"));
+        } catch (Exception e) {
+            return new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.facebook.com/<user_name_here>"));
+        }
+    }*/
 }
