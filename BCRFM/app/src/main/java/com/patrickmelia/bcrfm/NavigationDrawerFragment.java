e7fce5cf93cb512519d6ab4703d6338fd8390346
Patrick Melia <patrickmelia82@gmail.com>
2016-04-29 19:51:15 +0100

    Hopefully this will become version one
    Hopefully this will become version one :)

--- BCRFM/app/src/main/java/com/patrickmelia/bcrfm/NavigationDrawerFragment.java
+++ BCRFM/app/src/main/java/com/patrickmelia/bcrfm/NavigationDrawerFragment.java
@@ -119,7 +119,7 @@ public class NavigationDrawerFragment extends Fragment implements NavigationDraw
     public List<NavigationItem> getMenu() {
         List<NavigationItem> items = new ArrayList<NavigationItem>();
         items.add(new NavigationItem("Listen Live", getResources().getDrawable(R.mipmap.ic_radio)));
-        items.add(new NavigationItem("Schedule", getResources().getDrawable(R.drawable.ic_list_black_24dp)));
+        /*items.add(new NavigationItem("Schedule", getResources().getDrawable(R.drawable.ic_list_black_24dp)));*/
         items.add(new NavigationItem("BCRFM Website", getResources().getDrawable(R.drawable.ic_web_black_24dp)));
         items.add(new NavigationItem("About Us", getResources().getDrawable(R.mipmap.ic_info_black_24dp)));
         items.add(new NavigationItem("Contact Us", getResources().getDrawable(R.mipmap.ic_account_circle_black_24dp)));
